f0d9db33fa82935f1d8f5f94f3bde32720329aed
Manish Kumar Sharma <sharmamanishkumar558@gmail.com>
2025-08-10 14:52:14 +0530

    Fix styles.css import for Vercel build

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import './styles.css';
+
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store.js";
