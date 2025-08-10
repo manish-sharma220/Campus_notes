dd3bc892409d85cc80618ee7a630afc45fb3ca41
Manish Kumar Sharma <sharmamanishkumar558@gmail.com>
2025-08-10 14:54:46 +0530

    Fix wrong CSS import path for Vercel build

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './styles.css';
+import './index.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import Signin from "./Pages/Signin";
