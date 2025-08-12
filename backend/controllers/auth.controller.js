9eb60fe7771eff01e53e5947f702445e36e20b0f
Manish Kumar Sharma <sharmamanishkumar558@gmail.com>
2025-08-12 07:49:24 +0530

    updated secure flag in cookie

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -89,7 +89,7 @@ export const signin = async (req, res, next) => {
     res
       .cookie("access_token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        secure: true,
         sameSite: "none",
         maxAge: 3600000, // 1 hour
         path: "/",
